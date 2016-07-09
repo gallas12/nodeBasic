838e4e5ea03ed80d54981892fcf7efd18601ffdb
gallas <gallas@onio.cz>
2016-07-10 00:06:19 +0200

    Routing

--- app.js
+++ app.js
@@ -2,24 +2,6 @@
  * Created by Milan Gallas on 9.7.2016.
  */
 var http = require('http');
-var fs = require('fs'); //znázornění html
+var server = require('./server.js');
 
-
-function onRequest(request, response){
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./index.html', null, function(error, data){
-        if(error){
-            response.writeHead(404);
-            response.write('File not found');
-        }else{
-            response.write(data);
-        }
-        //response ukončím až zde ve vnořeném bloku. Ten vnější musí být zakomentován,
-        // jinak response končí dřív a nemůžu použít write metodu
-        response.end();
-    });
-    //ukončím hlavní blok
-    //response.end();
-}
-
-http.createServer(onRequest).listen(8000);
+http.createServer(server.handleRequest).listen(8000);
